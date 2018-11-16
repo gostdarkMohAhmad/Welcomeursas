26edafaf372b9c46838bb4ff498e01a1337d5cfc
gostdarkMohAhmad <44299639+gostdarkMohAhmad@users.noreply.github.com>
2018-11-16 21:25:45 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,9 @@
+client.on("guildMemberAdd", member => { 
+        if(member.guild.id === "500370868493352970") {  
+setTimeout(function(){ 
+  const channel = member.guild.channels.find('513072086839853056');  
+if (!channel) return;  
+  channel.send(`**Welcome To __DreamServer__  ** ❤️ `)  
+}, 5000);    
+}}); 
+client.login(process.env.BOT_TOKEN);
