69d0b1937e70993208a5a6f8e57d78d0af7edd7d
gostdarkMohAhmad <44299639+gostdarkMohAhmad@users.noreply.github.com>
2018-11-17 10:36:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ client.on('guildMemberAdd', member => {
 const mohamed= member.guild.channels.get("513072086839853056");
 if(!mohamed) return;
 if(mohamed) {
-setTimeout(() => mohamed.send(`**Welcome To __Dream__**`), 4000)        
+setTimeout(() => mohamed.send(`**Welcome To __Dream__ :revolving_hearts: **`), 4000)        
 }
 });
 client.login(process.env.BOT_TOKEN);
